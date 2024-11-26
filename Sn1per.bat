2eb0540b953c70917eb0b64f1428b6a82b1d821b
Skid-X60 <blakegerwing@gmail.com>
2024-11-26 14:08:13 -0500

    Add files via upload
    A network tool that shows you what is going on in your network.

---
+++ Sn1per.bat
@@ -0,0 +1,27 @@
+@echo off
+color 0b
+mode 47,20
+title Sn1per
+echo  Loading Network Information...
+:loop
+for /f "tokens=2 delims=:" %%a in ('netsh wlan show interface ^| find "SSID" ^| findstr /v "BSSID"') do set ssid=%%a
+for /f "tokens=2 delims=:" %%a in ('netsh wlan show interface ^| find "Description"') do set cord=%%a
+for /f "tokens=2 delims=:" %%a in ('netsh wlan show interface ^| find "State"') do set state=%%a
+for /f "tokens=2 delims=:" %%a in ('netsh wlan show interface ^| find "Signal"') do set signal=%%a
+for /f "tokens=4 delims==" %%a in ('ping -n 2 8.8.8.8 ^| find "Average"') do set ping=%%a
+for /f "tokens=2 delims= " %%a in ('netstat -e ^| find "Bytes"') do set rbytes=%%a
+for /f "tokens=3 delims= " %%a in ('netstat -e ^| find "Bytes"') do set sbytes=%%a
+cls
+echo  Network:
+echo  ========
+echo  SSID:%ssid%
+echo  NIC:%cord%
+echo  State:%state%
+echo  Signal:%signal%
+echo.
+echo  Speed:
+echo  ======
+echo  Ping:%ping%
+echo  Received: %rbytes%
+echo  Sent: %sbytes%
+goto  loop
